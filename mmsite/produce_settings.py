f4fba111a5bb337c976a537023e141927bb9aab4
xuchaoS <xuchaoshang@gmail.com>
2019-06-27 22:23:18 +0800

    re init

--- mmsite/produce_settings.py
+++ mmsite/produce_settings.py
@@ -1,18 +1,19 @@
-#!/usr/bin/env python
-# -*- encoding:utf-8 -*-
 """
 File Name: produce_settings
 Author: shangxc
 Created Time: 2019-06-27 21:55
 """
 # 导入公共配置
-from .settings import *
+from settings import *
 
 # 生产环境关闭DEBUG模式
 DEBUG = False
 
 # 生产环境关闭跨域
 CORS_ORIGIN_ALLOW_ALL = False
+
+ALLOWED_HOSTS = ['www.shangxuchao.com']
+
 
 if __name__ == '__main__':
     pass
